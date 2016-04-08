b0820cc916c7af69463e57b7a37f3d2a2a708c54
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 23:12:05 +0700

    Friendly message while syncing data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function getData(faskes) {
   var ref = new Firebase("https://cepatsembuh.firebaseio.com/" + faskes + '/pasien');
+  alert('Syncing data..');
   ref.once("value", function(snapshot) {
     var data = snapshot.numChildren();
     alert('The total is:' + data);
