8741629e517d2ecd5faa4ef01d460bb4439daa3f
Andre Christoga <andrechristoga@gmail.com>
2016-04-13 15:14:29 +0700

    Cleaner code using comments

--- js/add.js
+++ js/add.js
@@ -5,46 +5,56 @@ var newFaskes;
 
 function submitFaskes() {
   'use strict';
+  // Get input value
   var nama = $('#nama').val();
   var tipe = $('#tipe').val();
 
+  // If-else condition
   if (nama === '' || tipe === '') {
     alert('Input is not filled');
     return false;
   } else {
       switch (tipe) {
         case "puskesmas":
+            // Faskes depedencies
             faskes = ref.child("puskesmas");
             faskesVariable = faskes.child("pasien")
             newFaskes = faskesVariable.child(nama);
 
+            // Push data to firebase
             newFaskes.set({
               nama: nama
             })
           break;
         case "rsud":
+            // Faskes depedencies
             faskes = ref.child("rsud");
             faskesVariable = faskes.child("pasien")
             newFaskes = faskesVariable.child(nama);
 
+            // Push data to firebase
             newFaskes.set({
               nama: nama
             })
           break;
         case "rsuk":
+            // Faskes depedencies
             faskes = ref.child("rsuk");
             faskesVariable = faskes.child("pasien")
             newFaskes = faskesVariable.child(nama);
 
+            // Push data to firebase
             newFaskes.set({
               nama: nama
             })
           break;
         case "rss":
+            // Faskes depedencies
             faskes = ref.child("rss");
             faskesVariable = faskes.child("pasien")
             newFaskes = faskesVariable.child(nama);
 
+            // Push data to firebase
             newFaskes.set({
               nama: nama
             })
