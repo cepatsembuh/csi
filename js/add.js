4e0f9e54715852cfabfab15d6d818666b250b6fc
Andre Christoga <andrechristoga@gmail.com>
2016-04-13 15:06:31 +0700

    Push data to firebase

--- js/add.js
+++ js/add.js
@@ -1,9 +1,24 @@
 'use strict';
 var ref = new Firebase("https://cepatsembuh.firebaseio.com");
+var faskes;
+var newFaskes;
 
 function submitFaskes() {
   'use strict';
   var nama = $('#nama').val();
   var tipe = $('#tipe').val();
 
+  switch (tipe) {
+    case "puskesmas":
+        faskes = ref.child("puskesmas");
+        faskesVariable = faskes.child("pasien")
+        newFaskes = faskesVariable.child(nama);
+
+        newFaskes.set({
+          nama: nama
+        })
+      break;
+    default:
+
+  }
 }
