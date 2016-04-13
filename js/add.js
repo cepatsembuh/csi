73c8812c7392dc91fc27f4d41f4d2bb020dfa54f
Andre Christoga <andrechristoga@gmail.com>
2016-04-13 15:11:31 +0700

    Using if-else condtion

--- js/add.js
+++ js/add.js
@@ -8,42 +8,48 @@ function submitFaskes() {
   var nama = $('#nama').val();
   var tipe = $('#tipe').val();
 
-  switch (tipe) {
-    case "puskesmas":
-        faskes = ref.child("puskesmas");
-        faskesVariable = faskes.child("pasien")
-        newFaskes = faskesVariable.child(nama);
+  if (nama === '' || tipe === '') {
+    alert('Input is not filled');
+    return false;
+  } else {
+      switch (tipe) {
+        case "puskesmas":
+            faskes = ref.child("puskesmas");
+            faskesVariable = faskes.child("pasien")
+            newFaskes = faskesVariable.child(nama);
 
-        newFaskes.set({
-          nama: nama
-        })
-      break;
-    case "rsud":
-        faskes = ref.child("rsud");
-        faskesVariable = faskes.child("pasien")
-        newFaskes = faskesVariable.child(nama);
+            newFaskes.set({
+              nama: nama
+            })
+          break;
+        case "rsud":
+            faskes = ref.child("rsud");
+            faskesVariable = faskes.child("pasien")
+            newFaskes = faskesVariable.child(nama);
 
-        newFaskes.set({
-          nama: nama
-        })
-      break;
-    case "rsuk":
-        faskes = ref.child("rsuk");
-        faskesVariable = faskes.child("pasien")
-        newFaskes = faskesVariable.child(nama);
+            newFaskes.set({
+              nama: nama
+            })
+          break;
+        case "rsuk":
+            faskes = ref.child("rsuk");
+            faskesVariable = faskes.child("pasien")
+            newFaskes = faskesVariable.child(nama);
 
-        newFaskes.set({
-          nama: nama
-        })
-      break;
-    case "rss":
-        faskes = ref.child("rss");
-        faskesVariable = faskes.child("pasien")
-        newFaskes = faskesVariable.child(nama);
+            newFaskes.set({
+              nama: nama
+            })
+          break;
+        case "rss":
+            faskes = ref.child("rss");
+            faskesVariable = faskes.child("pasien")
+            newFaskes = faskesVariable.child(nama);
 
-        newFaskes.set({
-          nama: nama
-        })
-      break;
+            newFaskes.set({
+              nama: nama
+            })
+          break;
+      }
+    return true;
   }
 }
